d857c366839f1c8e418329cb69ab32b714d0daf7
Paulina <paulina.dorozko@gmail.com>
2023-11-21 11:23:34 +0100

    Add current weather

---
+++ WeatherApp/CurrentWeatherViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentWeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by Paulina Dorożko on 09/11/2023.
+//
+
+import Foundation
+
+class CurrentWeatherViewModel: ObservableObject {
+    
+}
